a9a6691f1c6c40fc389bc4d6a32fd9be5742214b
izumichika <chikara0707@outlook.jp>
2020-05-04 12:01:21 +0900

    変数化　整理

---  practiceReviewApp.rb
+++  practiceReviewApp.rb
@@ -1,13 +1,14 @@
 line = "----------------------"
-genre = "ジャンル :"
-title = "タイトル :"
-review = "感想 :"
-puts "ジャンル : 映画"
+genre = "映画"
+title = "タイトル : 時をかける少女"
+review = "人生の最高傑作アニメ。\n\n早く見たほうがいいよ。"
+
+puts "ジャンル" + genre
 puts line
-puts "タイトル : 時をかける少女"
+puts "タイトル" + title
 puts line
 puts "感想 :"
-puts "人生の最高傑作アニメ。\n\n早く見たほうがいいよ。"
+puts review
 puts line
 
 
